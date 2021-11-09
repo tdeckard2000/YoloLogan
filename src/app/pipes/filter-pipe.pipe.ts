f3dc68b1b008f298879cee27421e8502cd0793e9
Trent <tdeckard2000@yahoo.com>
2021-11-08 17:39:04 -0700

    Filter include description, company name, title, and host name. No longer case sensitive.

--- src/app/pipes/filter-pipe.pipe.ts
+++ src/app/pipes/filter-pipe.pipe.ts
@@ -9,20 +9,25 @@ export class FilterPipePipe implements PipeTransform {
   filteredObjects:Array<EventObject> = [] as Array<EventObject>;
 
   transform(objectArray: Array<EventObject>, searchString:string): Array<EventObject> {
-
     if(searchString.length < 2){
       return objectArray;
-    }
+    };
 
+    searchString = searchString.toLowerCase();
     this.filteredObjects = [] as Array<EventObject>;
 
     for(let object of objectArray){
-      if(object.title.includes(searchString)){
+      if(object.title.toLowerCase().includes(searchString)){
         this.filteredObjects.push(object);
-      }
-    }
+      }else if(object.description.toLowerCase().includes(searchString)){
+        this.filteredObjects.push(object);
+      }else if(object.address.businessName.toLowerCase().includes(searchString)){
+        this.filteredObjects.push(object);
+      }else if(object.contactName.toLowerCase().includes(searchString)){
+        this.filteredObjects.push(object);
+      };
+    };
 
-    console.log(this.filteredObjects)
     return this.filteredObjects;
   }
 
