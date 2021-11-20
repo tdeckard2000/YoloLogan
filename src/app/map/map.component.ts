117c992016f6add30ccdfe434ab176df483d3c75
Trent <tdeckard2000@yahoo.com>
2021-11-19 23:46:41 -0700

    Figure out how to add location pin to map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,9 +1,14 @@
-import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import {fromLonLat, transform} from 'ol/proj.js';
+import { Feature } from 'ol';
+import Point from 'ol/geom/Point';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import {Icon, Style} from 'ol/style';
 
 @Component({
   selector: 'app-map',
@@ -16,20 +21,47 @@ export class MapComponent implements OnInit {
 
   loganLonLat = [-111.830833, 41.737778];
   loganWebMercator = fromLonLat(this.loganLonLat);
+  map = {} as Map;
 
   ngOnInit(): void {
-    let map = new Map({
+    this.map = new Map({
       view: new View({
         // center: fromLonLat([41.7370, 111.8338]),
         center: this.loganWebMercator,
         zoom: 12,
       }),
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
       target: 'ol-map'
     });
+
+    let mainLayer = new TileLayer({
+      source: new OSM()
+    });
+
+    const rome = new Feature({
+      geometry: new Point(fromLonLat([-111.830833, 41.737778])),
+    });
+
+    rome.setStyle(
+      new Style({
+        image: new Icon({
+          color: '#ff0f00',
+      crossOrigin: 'anonymous',
+      // For Internet Explorer 11
+      imgSize: [50, 50],
+      src: 'assets/images/map-pin.svg',
+        })
+      })
+    )
+
+    const vectorSource = new VectorSource({
+      features: [rome]
+    });
+
+    const vectorLayer = new VectorLayer({
+      source: vectorSource
+    });
+
+    this.map.addLayer(mainLayer);
+    this.map.addLayer(vectorLayer);
   }
 }
