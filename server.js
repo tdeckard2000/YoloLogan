3488453e3a6422ad6ecebe4a458ffa8ea96b8cba
Trent <tdeckard2000@yahoo.com>
2021-12-01 23:02:22 -0700

    Work on connecting to MongoDB

--- server.js
+++ server.js
@@ -1,19 +1,59 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const { MongoClient } = require("mongodb");
+const { EBADF } = require('constants');
 require('dotenv').config();
 
-// Serve only the static files form the dist directory
+const port = process.env.PORT || 3000;
+const uri = process.env.MONGODB_CONNECTION_STRING;
+
+const client = new MongoClient(uri, {useNewUrlParser:true, useUnifiedTopology: true});
+
+async function runMongoDB() {
+  try {
+    await client.connect();
+    const db = client.db("YoloLogan");
+    console.warn("Connected successfully to server");
+
+    // const test = function(){
+    //   db.collection('events').find({title: "Dogs Splash Pool Day"}).toArray((err, res)=>{
+    //     console.log(res, err)
+    //     return res
+    //   });
+    // }
+
+    // app.get('/mytest', (req, res)=>{
+    //   console.log("HERE")
+    //   res =  db.collection('events').find({title: "Dogs Splash Pool Day"}).toArray((err, res)=>{
+    //     console.log(res, err)
+    //     return res
+    //   });
+    // })
+
+    // const test = await db.collection('events').findOne({title: "Dogs Splash Pool Day"})
+    // console.log(test)
+
+  } finally {
+    await client.close();
+  }
+}
+
+runMongoDB().catch(console.dir);
+
 app.use(express.static(__dirname + '/dist/yoloLogan'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req, res) {
 res.sendFile(path.join(__dirname+'/dist/yoloLogan/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+app.get('/test', async function (req, res){
+  await client.connect()
+  client.db("YoloLogan").collection('events').find({}).toArray((err, res)=>{
+    console.log(err, res)
+  })
+});
+
 app.listen(port, ()=>{
   console.warn("Listening on port " + port);
 });
-
-console.log(process.env.GOOGLE_API_KEY)
