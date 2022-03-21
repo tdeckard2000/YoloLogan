d3584730903ce99d7338504f71d44dd14a8764ee
Trent <tdeckard2000@yahoo.com>
2022-03-20 21:58:58 -0600

    Adjust spacing and borders on mobile events list

--- src/app/event-tile/event-tile.component.ts
+++ src/app/event-tile/event-tile.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { HttpService } from '../services/http.service';
 import { EventObject } from '../services/interfaces';
 import { MainService } from '../services/main.service';
+import { WindowSizeService } from '../services/window-size.service';
+
 
 @Component({
   selector: 'app-event-tile',
@@ -12,8 +14,9 @@ export class EventTileComponent implements OnInit {
 
   @Input() tileData:EventObject = {} as EventObject;
   @Input() searchString:string = 'Pool';
+  isMobile:boolean = false;
 
-  constructor(private httpService: HttpService, private mainService: MainService) { }
+  constructor(private httpService: HttpService, private mainService: MainService, private windowSizeService:WindowSizeService) { }
 
   events:Array<EventObject> = [];
 
@@ -21,13 +24,14 @@ export class EventTileComponent implements OnInit {
     this.httpService.getAllEvents().subscribe(results=> {
       this.events = results;
     })
-
     this.httpService.getFilteredEvents().subscribe(results=> {
       this.events = results;
     })
-
     this.mainService.getSearchString().subscribe(results=> {
       this.searchString = results;
+    })
+    this.windowSizeService.getIsMobile().subscribe(result => {
+      this.isMobile = result;
     })
   }
 
